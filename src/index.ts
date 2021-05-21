4837354bcae3ead9c31d5ecb4d5d1395da26e8ae
Robin van der Linde <robin.vanderlinde@gmail.com>
2021-05-21 10:54:37 +0200

    ✨ Add context menu

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,107 @@
 import "../node_modules/bootstrap/js/dist/collapse";
 
-(function () {
-  window.onload = function () {
-    let cityRow = document.getElementById("city-row");
-    const requestURL =
-      "https://raw.githubusercontent.com/EqualStreetNames/equalstreetnames/master/global/cities.json";
-    let request = new XMLHttpRequest();
-    request.open("GET", requestURL);
-    request.responseType = "json";
-    request.send();
+const openContextMenu = new Event("openContextMenu");
 
-    request.onload = function () {
-      const requestData = request.response;
-      const countries = Object.keys(requestData);
+let cityRow = document.getElementById("city-row");
 
-      let city;
+const statisticsBase = "https://statistics-guwfgh9bg-robinlinde.vercel.app/";
+const requestURL =
+  "https://raw.githubusercontent.com/EqualStreetNames/equalstreetnames/master/global/cities.json";
+let request = new XMLHttpRequest();
+request.open("GET", requestURL);
+request.responseType = "json";
+request.send();
 
-      for (var i = 0; i < countries.length; i++) {
-        var currentCountry = countries[i];
-        var cities = Object.keys(requestData[currentCountry]);
+request.onload = function () {
+  const requestData = request.response;
+  const countries = Object.keys(requestData);
 
-        var col = document.createElement("div");
-        col.className = "col-sm";
+  let city;
 
-        var listGroup = document.createElement("ul");
-        listGroup.className = "list-group";
-        col.appendChild(listGroup);
+  for (var i = 0; i < countries.length; i++) {
+    var currentCountry = countries[i];
+    var cities = Object.keys(requestData[currentCountry]);
 
-        var listGroupHeading = document.createElement("li");
-        listGroupHeading.className = "list-group-item list-group-heading";
-        listGroupHeading.innerText = requestData[currentCountry][cities[0]][
-          "name"
-        ].substr(0, 4);
-        listGroup.appendChild(listGroupHeading);
+    var col = document.createElement("div");
+    col.className = "col-sm";
 
-        console.log(currentCountry);
+    var listGroup = document.createElement("ul");
+    listGroup.className = "list-group";
+    col.appendChild(listGroup);
 
-        for (var j = 0; j < cities.length; j++) {
-          var currentCity = cities[j];
+    var listGroupHeading = document.createElement("li");
+    listGroupHeading.className = "list-group-item list-group-heading";
+    listGroupHeading.innerText = requestData[currentCountry][cities[0]][
+      "name"
+    ].substr(0, 4);
+    listGroup.appendChild(listGroupHeading);
 
-          city = document.createElement("a");
-          city.className = "list-group-item city-link";
-          city.text = requestData[currentCountry][currentCity]["name"]
-            .split(",")[0]
-            .substr(4);
-          city.href = requestData[currentCountry][currentCity]["url"];
-          listGroup.appendChild(city);
-        }
+    for (var j = 0; j < cities.length; j++) {
+      var currentCity = cities[j];
 
-        cityRow.appendChild(col);
-      }
-      document.body.classList.add("loaded");
-    };
-  };
-})();
+      city = document.createElement("a");
+      city.className = "list-group-item city-link";
+      city.text = requestData[currentCountry][currentCity]["name"]
+        .split(",")[0]
+        .substr(4);
+      city.href = requestData[currentCountry][currentCity]["url"];
+      city.addEventListener("contextmenu", openMenu, false);
+      city.addEventListener("openContextMenu", closeMenuOnOpen, false);
+      listGroup.appendChild(city);
+
+      var dropdown = document.createElement("ul");
+      dropdown.className = "dropdown-menu";
+      dropdown.style.display = "none";
+      dropdown.addEventListener("mouseleave", closeMenu, false);
+      city.appendChild(dropdown);
+
+      var open = document.createElement("a");
+      open.className = "dropdown-item";
+      open.href = requestData[currentCountry][currentCity]["url"];
+      open.innerText = "Open";
+      dropdown.appendChild(open);
+
+      var openBlank = document.createElement("a");
+      openBlank.className = "dropdown-item";
+      openBlank.href = requestData[currentCountry][currentCity]["url"];
+      openBlank.target = "_blank";
+      openBlank.innerText = "Open in new tab";
+      dropdown.appendChild(openBlank);
+
+      var openStatistics = document.createElement("a");
+      openStatistics.className = "dropdown-item";
+      openStatistics.href =
+        statisticsBase + "?city=" + countries[i] + "/" + cities[j];
+      openStatistics.innerText = "Open statistics";
+      dropdown.appendChild(openStatistics);
+
+      var openStatisticsBlank = document.createElement("a");
+      openStatisticsBlank.className = "dropdown-item";
+      openStatisticsBlank.href =
+        statisticsBase + "?city=" + countries[i] + "/" + cities[j];
+      openStatisticsBlank.target = "_blank";
+      openStatisticsBlank.innerText = "Open statistics in new tab";
+      dropdown.appendChild(openStatisticsBlank);
+    }
+
+    cityRow.appendChild(col);
+  }
+  document.body.classList.add("loaded");
+};
+
+function openMenu() {
+  var cityLinks = document.getElementsByClassName("city-link");
+  for (var i = 0; i < cityLinks.length; i++) {
+    cityLinks.item(i).dispatchEvent(openContextMenu);
+  }
+  //document.dispatchEvent(openContextMenu);
+  this.firstElementChild.style.display = "block";
+}
+
+function closeMenu() {
+  this.style.display = "none";
+}
+
+function closeMenuOnOpen() {
+  this.firstElementChild.style.display = "none";
+}
